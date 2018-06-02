cfdd91711279a6259bac4e3289c6f11808582532
Vladyslav <raven73093@gmail.com>
2018-06-02 17:36:18 +0300

    Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+$( document ).ready( function() {
+  var slideShow = function( event ) {
+    console.log( 'app.js' );
+    var direction = 'next';
+    if( event && $( event.target ).is( '.prev' ) ) {
+      direction = 'prev';
+    }
+    if( direction === 'next' ) {
+      console.log( 'next' );
+      $( '.area' ).first().insertAfter( '.area:last-child' );
+    }
+    if( direction === 'prev' ) {
+      console.log( 'prev' );
+      $( '.area' ).last().insertBefore( '.area:first-child' );
+    }
+  };
+  $( '.arrows i' ).click( slideShow );
+
+  // setInterval( slideShow, 2000 );
+})
