d3bc766647851351826a4e2a7960c4298f594630
Vladyslav Rechkoblit <34210967+VladySlav7309@users.noreply.github.com>
2018-06-03 14:57:43 +0300

    From grid to flex

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
 $( document ).ready( function() {
   var slideShow = function( event ) {
-    console.log( 'app.js' );
     var direction = 'next';
     if( event && $( event.target ).is( '.prev' ) ) {
       direction = 'prev';
     }
-    if( direction === 'next' ) {
-      console.log( 'next' );
-      $( '.area' ).first().insertAfter( '.area:last-child' );
-    }
-    if( direction === 'prev' ) {
-      console.log( 'prev' );
-      $( '.area' ).last().insertBefore( '.area:first-child' );
-    }
-  };
-  $( '.arrows i' ).click( slideShow );
+    $( '.carousel' ).children().addClass( 'transition_65' );
+        if( direction === 'next' ) {
+            let count = Math.round( $( '.carousel' ).width() / $( '.area' ).first().width() ) - 1;
 
-  setInterval( slideShow, 2000 );
+            if( ( $( '.carousel' ).children().length / 2 ) < count ) {
+                count = $( '.carousel' ).children().length - count -1;
+            }
+
+            let ratio = ( $( '.area' ).first().width() / $( '.carousel' ).width() * 10);
+            let margin = '-' + ( count * ( ratio * 10 + 1 ) - 0.5 )  + '%';
+
+            $( '.area' ).first().css( 'margin-right', margin ).delay( 650 ).queue( function() {
+                $.each( $( '.area' ), function( index, area ) {
+                    if( index < count ) {
+                          $( area ).insertAfter( '.area:last-child' ).css( 'margin-right', '' ).dequeue();
+                    }
+                });
+                $( '.carousel' ).children().removeClass( 'transition_65' );
+            });
+        }
+        if( direction === 'prev' ) {
+            let count = Math.round( $( '.carousel' ).width() / $( '.area' ).first().width() ) - 1;
+
+            if( ( $( '.carousel' ).children().length / 2 ) < count ) {
+                count = $( '.carousel' ).children().length - count -1;
+            }
+
+            let ratio = ( $( '.area' ).first().width() / $( '.carousel' ).width() * 10);
+            let margin = '-' + ( count * ( ratio * 10 + 1) - 0.5 )  + '%';
+            var areas = $( '.area' ).get().reverse();
+            var number = $( '.carousel' ).children().length - count;
+            var last = $( '.carousel' ).children()[ number];
+
+            $( last ).css( 'margin-right', margin ).queue( function() {
+                $.each( areas, function( index, area ) {
+                        if( index < count ) {
+                              $( area ).insertBefore( '.area:first-child' ).dequeue();
+                        }
+                });
+            }).delay().queue( function() {
+                $( last ).css( 'margin-right', '' ).delay( 650 ).queue( function() {
+                    $( '.carousel' ).children().removeClass( 'transition_65' ).dequeue();
+                }).dequeue();
+            });
+
+        }
+  };
+
+  $( '.arrows i' ).click( slideShow );
 })
